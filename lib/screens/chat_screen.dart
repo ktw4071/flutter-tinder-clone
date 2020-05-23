f40ac898eff2aca7d944c19975e673c768f03d10
Taewoo Kim <taewkim@cisco.com>
2020-05-23 12:03:29 -0400

    make chat screen

---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,250 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.pinkAccent,
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.person),
+                iconSize: 32,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite),
+                iconSize: 32,
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              IconButton(
+                icon: Icon(Icons.chat_bubble),
+                iconSize: 32,
+                onPressed: () {},
+              ),
+            ]),
+        ),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Text('New Matches'),
+              Expanded(
+                flex: 2,
+                child: ListView(
+                  shrinkWrap: true,
+                  scrollDirection: Axis.horizontal,
+                  children: const <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.pinkAccent,
+                        child: CircleAvatar(
+                          child: Text('100+'),
+                          backgroundColor: Colors.amber,
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.blue,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/wonho.jpg'),
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.blueAccent,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/wonho.jpg'),
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.blue,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/wonho.jpg'),
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.blue,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/wonho.jpg'),
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.blue,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/wonho.jpg'),
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: CircleAvatar(
+                        radius: 43,
+                        backgroundColor: Colors.blue,
+                        child: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/wonho.jpg'),
+                          radius: 40,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Text('Messages'),
+              Expanded(
+                flex: 8,
+                child: ListView(shrinkWrap: true, children: const <Widget>[
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: AssetImage('assets/images/wonho.jpg'),
+                      ),
+                      title: Text('Wonho',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/jimin.jpg')),
+                      title: Text('Jimin',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/Minhyuk.jpg')),
+                      title: Text('Minhyuk',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/rain.jpg')),
+                      title: Text('Rain',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/taeyang.jpg')),
+                      title: Text('Taeyang',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/gdragon.jpeg')),
+                      title: Text('GD',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/shown.jpg')),
+                      title: Text('Shown',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                  Card(
+                    child: ListTile(
+                      leading: CircleAvatar(
+                          backgroundImage:
+                              AssetImage('assets/images/chanyeol.jpg')),
+                      title: Text('Chanyeol',
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              fontFamily: 'Open Sans',
+                              fontSize: 16)),
+                      subtitle: Text('Sup?'),
+                    ),
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ));
+  }
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+import 'chat_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.pinkAccent,
+        title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              IconButton(
+                icon: Icon(Icons.person),
+                iconSize: 32,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.favorite),
+                iconSize: 32,
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.chat_bubble),
+                iconSize: 32,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => ChatScreen()),
+                  );
+                },
+              ),
+            ]),
+      ),
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+                flex: 65,
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 10.0, right: 10.0, top: 20.0, bottom: 20.0),
+                  child: Stack(fit: StackFit.expand, children: <Widget>[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(8.0),
+                      child: Image.asset(
+                        'assets/images/gd.jpg',
+                        fit: BoxFit.fitHeight,
+                      ),
+                    ),
+                    Positioned(
+                        bottom: 55,
+                        left: 15,
+                        child: Row(
+                          children: <Widget>[
+                            Text('GD',
+                                style: TextStyle(
+                                    color: Colors.grey[200],
+                                    fontWeight: FontWeight.w800,
+                                    fontFamily: 'Open Sans',
+                                    fontSize: 36)),
+                            Container(padding: EdgeInsets.only(left: 15.0)),
+                            Text('31',
+                                style: TextStyle(
+                                    color: Colors.grey[200],
+                                    fontWeight: FontWeight.w400,
+                                    fontFamily: 'Open Sans',
+                                    fontSize: 25)),
+                          ],
+                        )),
+                    Positioned(
+                        bottom: 25,
+                        left: 15,
+                        child: Row(
+                          children: <Widget>[
+                            Text('YG Entertainment',
+                                style: TextStyle(
+                                    color: Colors.grey[200],
+                                    fontWeight: FontWeight.w300,
+                                    fontFamily: 'Open Sans',
+                                    fontSize: 20)),
+                          ],
+                        )),
+                    Positioned(
+                        bottom: 25,
+                        right: 15,
+                        child: IconButton(
+                            icon: Icon(Icons.info),
+                            iconSize: 32,
+                            color: Colors.grey[200],
+                            onPressed: () {}))
+                  ]),
+                )),
+            Expanded(
+              flex: 10,
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Container(padding: EdgeInsets.only(left: 8.0)),
+                    IconButton(
+                      icon: Icon(Icons.undo),
+                      iconSize: 32,
+                      color: Colors.amber,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.clear),
+                      iconSize: 48,
+                      color: Colors.red,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.star),
+                      iconSize: 32,
+                      color: Colors.blue,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.favorite),
+                      iconSize: 48,
+                      color: Colors.green,
+                      onPressed: () {},
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.hourglass_full),
+                      iconSize: 32,
+                      color: Colors.purple,
+                      onPressed: () {},
+                    ),
+                    Container(padding: EdgeInsets.only(right: 8.0)),
+                  ]),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
